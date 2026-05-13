bb55050c844ec0aadc4337715773e87bd8e6ce8e
Alex <01210133@pw.edu.pl>
2026-05-13 16:48:04 +0200

    integrate load_bmp_pixels and update diagnostic prints

--- main.s
+++ main.s
@@ -3,18 +3,29 @@
 newline:.asciz	"\n"
 msg_w:	.asciz 	"width: "
 msg_h:	.asciz "height: "
-msg_p:	.asciz	"pixel offset: "
+msg_p:	.asciz	"pixel address on the heap: "
+msg_s:	.asciz	"stride: "
 
 .text
 main:
-	jal	read_bmp_metadata
+	jal	load_bmp_pixels
 	
 	bnez	a0, fin
+	
+	la	a0, msg_p
+	li	a7, SYS_PRINT_STR	# print pixel address info
+	ecall
+	mv	a0, a0
+	li	a7, SYS_PRINT_INT
+	ecall
+	la	a0, newline
+	li	a7, SYS_PRINT_STR
+	ecall
 	
 	la	a0, msg_w
 	li	a7, SYS_PRINT_STR	# print width info
 	ecall
-	mv	a0, a1
+	mv	a0, a2
 	li	a7, SYS_PRINT_INT
 	ecall
 	la	a0, newline
@@ -24,17 +35,17 @@ main:
 	la	a0, msg_h
 	li	a7, SYS_PRINT_STR	# print height info
 	ecall
-	mv	a0, a2
+	mv	a0, a3
 	li	a7, SYS_PRINT_INT
 	ecall
 	la	a0, newline
 	li	a7, SYS_PRINT_STR
 	ecall
 	
-	la	a0, msg_p
-	li	a7, SYS_PRINT_STR	# print pixel offset info
+	la	a0, msg_s
+	li	a7, SYS_PRINT_STR	# print stride info
 	ecall
-	mv	a0, a3
+	mv	a0, a4
 	li	a7, SYS_PRINT_INT
 	ecall
 	la	a0, newline
