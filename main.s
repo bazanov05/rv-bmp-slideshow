22b281d708ccc0cb747f38a956c03b7e7af64d23
Alex <01210133@pw.edu.pl>
2026-05-14 16:23:27 +0200

    print height;s sign and save return arguments in s registors

--- main.s
+++ main.s
@@ -1,51 +1,67 @@
 	.include	"syscalls.s"
 .data
 newline:.asciz	"\n"
-msg_w:	.asciz 	"width: "
-msg_h:	.asciz "height: "
-msg_p:	.asciz	"pixel address on the heap: "
-msg_s:	.asciz	"stride: "
-
+msg_width:	.asciz 	"width: "
+msg_height:	.asciz "height: "
+msg_pixel:	.asciz	"pixel address on the heap: "
+msg_stride:	.asciz	"stride: "
+msg_sign:	.asciz	"height's sign: "
 .text
 main:
 	jal	load_bmp_pixels
 	
 	bnez	a0, fin
 	
-	la	a0, msg_p
+	mv	s0, a1		# pixel address on heap
+	mv	s1, a2		# width
+	mv	s2, a3		# height
+	mv	s3, a4		# stride
+	mv	s4, a5		# heigh's sign
+	
+	la	a0, msg_pixel
 	li	a7, SYS_PRINT_STR	# print pixel address info
 	ecall
-	mv	a0, a0
+	mv	a0, s0
 	li	a7, SYS_PRINT_INT
 	ecall
 	la	a0, newline
 	li	a7, SYS_PRINT_STR
 	ecall
 	
-	la	a0, msg_w
+	la	a0, msg_width
 	li	a7, SYS_PRINT_STR	# print width info
 	ecall
-	mv	a0, a2
+	mv	a0, s1
 	li	a7, SYS_PRINT_INT
 	ecall
 	la	a0, newline
 	li	a7, SYS_PRINT_STR
 	ecall
 	
-	la	a0, msg_h
+	la	a0, msg_height
 	li	a7, SYS_PRINT_STR	# print height info
 	ecall
-	mv	a0, a3
+	mv	a0, s2
 	li	a7, SYS_PRINT_INT
 	ecall
 	la	a0, newline
 	li	a7, SYS_PRINT_STR
 	ecall
 	
-	la	a0, msg_s
+	la	a0, msg_stride
 	li	a7, SYS_PRINT_STR	# print stride info
 	ecall
-	mv	a0, a4
+	mv	a0, s3
+	li	a7, SYS_PRINT_INT
+	ecall
+	la	a0, newline
+	li	a7, SYS_PRINT_STR
+	ecall
+	
+	la	a0, msg_sign
+	li	a7, SYS_PRINT_STR	# print info about height's sign
+	ecall
+	mv	a0, s4
 	li	a7, SYS_PRINT_INT
 	ecall
 	la	a0, newline
