b13e6ac0e91579139f8c45c96f0cb1943df14965
Alex <01210133@pw.edu.pl>
2026-05-19 19:51:59 +0200

    migrate to struct-based state and optimize animation loop

--- main.s
+++ main.s
@@ -1,10 +1,12 @@
 	.include	"syscalls.s"
 .data
 step:	.word	0
-cx1:	.word	0	# center_x for pic1
-cy1:	.word	0	# center_y for pic1
+cx1:	.word	0
+cy1:	.word	0
 cx2:	.word	0
 cy2:	.word	0
+pic1:	.space	28	# 7 parameters - pixel address, width, height, stride, height's sign, x_begin, y_begin
+pic2:	.space	28
 swap_done:	.byte	0	# flag - to chceck if the swap between pics was done
 .text
 main:
@@ -18,149 +20,164 @@ main:
 	ecall
 	
 	mv	a0, s11		# get 1st file data
+	la	a1, pic1
 	jal	load_bmp_pixels
 	
 	bnez	a0, fin
-	
-	mv	s0, a1		# 1st pixel address on heap
-	mv	s1, a2		# 1st width
-	mv	s2, a3		# 1st height
-	mv	s3, a4		# 1st stride
-	mv	s4, a5		# 1st height's sign
 	
 	mv	a0, s10		# get 2nd file data
+	la	a1, pic2
 	jal	load_bmp_pixels
 	
 	bnez	a0, fin
 	
-	mv	s5, a1		# 2nd pixel address on heap
-	mv	s6, a2		# 2nd width
-	mv	s7, a3		# 2nd height
-	mv	s8, a4		# 2nd stride
-	mv	s9, a5		# 2nd height's sign
-	
-	mv	a0, s1
+	la	t0, pic1
+	lw	a0, 4(t0)
 	jal	calculate_x_offset
 	la	t0, cx1
 	sw	a0, 0(t0)		
 	
-	mv	a0, s2
+	la	t0, pic1
+	lw	a0, 8(t0)
 	jal	calculate_y_offset
 	la	t0, cy1
 	sw	a0, 0(t0)		
 	
-	mv	a0, s6
+	la	t0, pic2
+	lw	a0, 4(t0)
 	jal	calculate_x_offset
 	la	t0, cx2
-	sw	a0, 0(t0)
+	sw	a0, 0(t0)		
 	
-	mv	a0, s7
+	la	t0, pic2
+	lw	a0, 8(t0)
 	jal	calculate_y_offset
 	la	t0, cy2
 	sw	a0, 0(t0)	
 	
 animation_loop:
 	la	t0, step
-	sw	zero, 0(t0)	# reset step before next drawning 
+	sw	zero, 0(t0)	# reset step before next drawing 
 	
 slide_loop:
-	jal	clear_screen	# clean the screen before every loop so on the screen there will be no old drawings
+
+	jal	clear_screen	# clean the screen before every loop
+
 	la	t0, cx1
 	la	t1, step
 	lw	t2, 0(t0)
 	lw	t3, 0(t1)
-	sub	a6, t2, t3	# x_curr = x_center - step
+	sub	t4, t2, t3	# x_curr = x_center - step
+
+	la	t0, pic1
+	sw	t4, 20(t0)	# update x_begin inside the struct for pic1
+
 	
 	la	t0, cy1
-	lw	a7, 0(t0)	# we always have height's alighment (y)
-	
-	mv	a1, s0		# pixel address
-	mv	a2, s1		# pic's width
-	mv	a3, s2		# pic's height
-	mv	a4, s3		# pic's stride
-	mv	a5, s4		# height's sigh
-	
+	lw	t4, 0(t0)	# we always have height's alignment (y)
+	la	t0, pic1
+	sw	t4, 24(t0)	# update y_begin inside the struct for pic1
+
+	la	a0, pic1	# pass address of the pic1 struct
 	jal	draw_image	# draw the first pic
-	
+
+
 	la	t0, cx2
 	la	t1, step
 	lw	t2, 0(t0)
 	lw	t3, 0(t1)
-	sub	a6, t2, t3	# x_curr = x_center - offset
-	addi	a6, a6, SCREEN_WIDTH	# x_curr = x_center - offset + 512 , 2nd pic starts from the right
+	sub	t4, t2, t3	
+	addi	t4, t4, SCREEN_WIDTH	# x_curr = x_center - step + 512 (starts from the right)
+
 	
+	la	t0, pic2
+	sw	t4, 20(t0)	# update x_begin inside the struct for pic2
+
 	la	t0, cy2
-	lw	a7, 0(t0)
-	
-	mv	a1, s5		# pixel address
-	mv	a2, s6		# pic's width
-	mv	a3, s7		# pic's height
-	mv	a4, s8		# pic's stride
-	mv	a5, s9		# height's sigh
-	
+	lw	t4, 0(t0)
+	la	t0, pic2
+	sw	t4, 24(t0)	# update y_begin inside the struct for pic2
+
+	la	a0, pic2	# pass address of the pic2 struct
 	jal	draw_image
-	
-	li	a0, 2		# sleep for 2 ms
+
+
+	li	a0, 20		# sleep for 20 ms
 	li	a7, SYS_SLEEP
+
 	ecall
+
 	
+
 	la	t0, step
 	lw	t3, 0(t0)
-	addi	t3, t3, 128	# incr step
+	addi	t3, t3, 128	# incr step by 128 (1/4 of the screen)
 	sw	t3, 0(t0)
+
 	
 	li	t4, SCREEN_WIDTH
 	ble	t3, t4, slide_loop
+
 	
 	la	t0, swap_done
 	lbu	t1, 0(t0)
 	bnez	t1, fin		# if the swap was done - end the program 
-	
+
 	addi	t1, t1, 1
 	sb	t1, 0(t0)
+
 	
+
 swap:
-	mv	t0, s0
-	mv	s0, s5
-	mv	s5, t0		# swap pixel addresses
+
+	la	t0, pic1
+	la	t1, pic2
+	li	t2, 5		# counter for 5 words, there is no need to swap x and y because loop will overwrite em
+
 	
-	mv	t0, s1
-	mv	s1, s6
-	mv	s6, t0		# swap width
 
-	mv	t0, s2
-	mv	s2, s7
-	mv	s7, t0		# swap height
+swap_struct_loop:
+	lw	t3, 0(t0)	# load word from pic1
+	lw	t4, 0(t1)	# load word from pic2
+	sw	t4, 0(t0)	# store pic2 word into pic1
+	sw	t3, 0(t1)	# store pic1 word into pic2
 
-	mv	t0, s3
-	mv	s3, s8
-	mv	s8, t0		# swap stride
+	addi	t0, t0, 4	# move to next word
+	addi	t1, t1, 4
 
-	mv	t0, s4
-	mv	s4, s9
-	mv	s9, t0		# swap sign's
+	addi	t2, t2, -1	# decr counter
+
+	bnez	t2, swap_struct_loop
 
 	la	t0, cx1
 	lw	t1, 0(t0)
 	la	t2, cx2
 	lw	t3, 0(t2)
+
 	sw	t3, 0(t0)
 	sw	t1, 0(t2)	# swap x_center
+
+
 
 	la	t0, cy1
 	lw	t1, 0(t0)
 	la	t2, cy2
 	lw	t3, 0(t2)
+
 	sw	t3, 0(t0)
 	sw	t1, 0(t2)	# swap y_center
+
 	
-	li	a0, 2000	# sleep for 2000 ms
+
+	li	a0, 2000	# sleep for 2000 ms before reverse animation
 	li	a7, SYS_SLEEP
 	ecall
 	
 	j	animation_loop
+
 fin:
-	li	a7, SYS_EXIT0		# end the program
+
+	li	a7, SYS_EXIT0	# end the program
 	ecall	
 	
 calculate_y_offset:
