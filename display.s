1f786e1b50222971d31394fb564e3382f909f691
Alex <01210133@pw.edu.pl>
2026-05-19 19:52:14 +0200

    eliminate stack frame and optimize pixel conversion

--- display.s
+++ display.s
@@ -3,55 +3,43 @@
 	.globl		clear_screen
 .text
 draw_image:
-	addi	sp, sp, -48
-	sw	ra, 44(sp)
-	sw	s0, 40(sp)	# x_begin - where to start drawing 
-	sw	s1, 36(sp)	# y_begin - always alighned 
-	sw	s2, 32(sp)
-	sw	s3, 28(sp)
-	sw	s4, 24(sp)
-	sw	s5, 20(sp)
-	sw	s6, 16(sp)
-	sw	s7, 12(sp)
+	lw	t0, 0(a0)	# pixel address on heap
+	lw	a4, 20(a0)	# x_begin
+	lw	a5, 4(a0)	# width
+	lw	a6, 12(a0)	# stride
+	lw	t1, 24(a0)	# y_begin - in the loops curr_y
+	lw	t3, 8(a0)	# height - in the loops height_counter for outer_loop
 	
-	mv	s0, a6		# x_begin
-	mv	s1, a7		# y_offset
-	mv	s2, a2		# pic width
-	mv	s3, a3		# pic height
-	mv	s4, a4		# stride
-	mv	s5, a5		# height's sign
-	mv	s6, a1		# pixel offset - the address of pixel bytes on the heap
-		
-	mv	s7, s3		# outer loop counter - height
+	lw	a1, 16(a0)	# height's sign
 	
-	add	t1, s1, s3
-	addi	t1, t1, -1	# start from bottom, in t1 i have curr_y
-	li	t2, -1		# in t2 i have a loop step - if we start on bottom we should incr
-	li   	t0, 0x10040000      # load base heap address
+	mv	a2, t1		# copy y_start
+	li	a7, 0x10040000	# base heap address
 	
-	beqz	s5, outer_loop	# if the sigh on height is positive - start drawing from bottom 
+	add	t1, t1, t3
+	addi	t1, t1, -1	# curr_y = y_begin + height - 1
+	li	t2, -1		# y_step = -1, because we start from the down
 	
-	mv	t1, s1		# if the height is negative - draw from top
-	li	t2, 1		# step is 1 - incr
+	beqz	a1, outer_loop	# if sign is positive - draw from the bottom
+	
+	mv	t1, a2
+	li	t2, 1		# if not - draw from the top and y_step = 1
 
 outer_loop:
-	mv	t4, s0		# x_curr = x_begin 
-	mv	t5, s2		# counter for inner loop is width - how many pixels we should draw in this row
-	mv	t6, s6		# save old pixel addres, so after reading one row add stride to it to come to a new one
+	mv	t4, a4		# x_curr = x_begin 
+	mv	t5, a5		# counter for inner loop is width - how many pixels we should draw in this row
+	mv	t6, t0		# save old pixel addres, so after reading one row add stride to it to come to a new one
 	
 inner_loop:
-	lbu	a0, 0(s6)	# blue
-	lbu	a1, 1(s6)	# green
-	lbu	a2, 2(s6)	# red
-	
-	slli	a2, a2, 16
-	slli	a1, a1, 8
-	
+	lbu	a0, 2(t0)	# red
+	slli	a0, a0, 8
+	lbu	a1, 1(t0)	# green
 	or	a0, a0, a1
-	or	a0, a0, a2	# change BGR format to RGB
+	slli	a0, a0, 8
+	lbu	a1, 0(t0)	# blue
+	or	a0, a0, a1	# change BGR format to RGB
+
 	
-	
-	addi	s6, s6, 3	# go to next 3 bytes to read next pixel
+	addi	t0, t0, 3	# go to next 3 bytes to read next pixel
 	
 	bltz	t4, skip_pixel	# if we are out of screen from the left - do not draw pixel
 	li	a3, SCREEN_WIDTH
@@ -61,13 +49,13 @@ inner_loop:
 	li      a3, SCREEN_HEIGHT
 	bge     t1, a3, skip_pixel	# if y >= screen height - skip pixel
 	 
-	slli	a7, t1, 9      	# a7 = y * 512
-	add	a7, a7, t4      # a7 = (y * 512) + x
-	slli	a7, a7, 2       # a7 = ((y * 512) + x) * 4 
+	slli	a2, t1, 9      	# a2 = y * 512
+	add	a2, a2, t4      # a2 = (y * 512) + x
+	slli	a2, a2, 2       # a2 = ((y * 512) + x) * 4 
 	
-	add	a7, a7, t0      # a7 = base address + offset
+	add	a2, a2, a7      # a2 = base display address + offset
 	
-	sw	a0, 0(a7)       # show pixel on the screen
+	sw	a0, 0(a2)       # show pixel on the screen
 	
 skip_pixel:
 	addi	t5, t5, -1
@@ -75,27 +63,16 @@ skip_pixel:
 	
 	bnez	t5, inner_loop
 	
-	add	t6, t6, s4	# add to current pixel address stride to come to new row
-	mv	s6, t6		# update pixel start
+	add	t6, t6, a6	# add to current pixel address stride to come to new row
+	mv	t0, t6		# update pixel start
 	
 	add	t1, t1, t2	# update row - either incr or decr(depending on t2, which step based on height's sign)
 
-	addi	s7, s7, -1	# update the outer loop counter which is height
+	addi	t3, t3, -1	# update the outer loop counter which is height
 	
-	bnez	s7, outer_loop
+	bnez	t3, outer_loop
 	
 epilogue:
-	lw	s7, 12(sp)
-	lw	s6, 16(sp)
-	lw	s5, 20(sp)
-	lw	s4, 24(sp)
-	lw	s3, 28(sp)
-	lw	s2, 32(sp)
-	lw	s1, 36(sp)
-	lw	s0, 40(sp)
-	lw	ra, 44(sp)
-	addi	sp, sp, 48
-	
 	ret
 	
 clear_screen:
